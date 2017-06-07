e5bd27b387a91067df964de1b72a1557be148660
Kristof Majoros <akmajoros1@gmail.com>
2017-06-07 14:12:35 +0200

    a3

--- src/Marci.java
+++ src/Marci.java
@@ -1,9 +1,10 @@
 public class Marci {
 
   private final static int MY_AGE = 33;
+  private final static String BIRTH_MONTH = "June";
 
   public void count() {
-    System.out.println(MY_AGE);
+    System.out.println(BIRTH_MONTH);
   }
 
 }
